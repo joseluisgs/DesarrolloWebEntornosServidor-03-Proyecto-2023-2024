45f5e5206aa503effecfa3bbb3c49616aaf60ba3
José Luis González Sánchez <joseluisgonsan@gmail.com>
2023-11-15 14:19:35 +0100

    servicio de token y darse de alta

--- tienda-api-nest-js/src/rest/auth/auth.module.ts
+++ tienda-api-nest-js/src/rest/auth/auth.module.ts
@@ -3,9 +3,25 @@ import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
 import { UsersModule } from '../users/users.module'
 import { AuthMapper } from './mappers/usuarios.mapper'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    // Configuración edl servicio de JWT
+    JwtModule.register({
+      // Lo voy a poner en base64
+      secret: Buffer.from(
+        process.env.TOKEN_SECRET ||
+          'Me_Gustan_Los_Pepinos_De_Leganes_Porque_Son_Grandes_Y_Hermosos',
+        'utf-8',
+      ).toString('base64'),
+      signOptions: {
+        expiresIn: Number(process.env.TOKEN_EXPIRES) || 3600, // Tiempo de expiracion
+        algorithm: 'HS512', // Algoritmo de encriptacion
+      },
+    }),
+    UsersModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService, AuthMapper],
 })
--- tienda-api-nest-js/src/rest/auth/auth.service.ts
+++ tienda-api-nest-js/src/rest/auth/auth.service.ts
@@ -1,8 +1,14 @@
-import { Injectable, Logger } from '@nestjs/common'
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common'
 import { UserSignUpDto } from './dto/user-sign.up.dto'
 import { UserSignInDto } from './dto/user-sign.in.dto'
 import { UsersService } from '../users/users.service'
 import { AuthMapper } from './mappers/usuarios.mapper'
+import { UserDto } from '../users/dto/user-response.dto'
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class AuthService {
@@ -11,18 +17,39 @@ export class AuthService {
   constructor(
     private readonly usersService: UsersService,
     private readonly authMapper: AuthMapper,
+    private readonly jwtService: JwtService,
   ) {}
 
   async singUp(userSignUpDto: UserSignUpDto) {
     this.logger.log(`singUp ${userSignUpDto.username}`)
 
-    return await this.usersService.create(
+    const user = await this.usersService.create(
       this.authMapper.toCreateDto(userSignUpDto),
     )
+    return this.getAccessToken(user)
   }
 
   async singIn(userSignInDto: UserSignInDto) {
     this.logger.log(`singIn ${userSignInDto.username}`)
     return userSignInDto
+  }
+
+  // TODO!!!
+
+  private getAccessToken(userDto: UserDto) {
+    this.logger.log(`getAccessToken ${userDto.username}`)
+    try {
+      const payload = {
+        id: userDto.id,
+      }
+      //console.log(payload)
+      const access_token = this.jwtService.sign(payload)
+      return {
+        access_token,
+      }
+    } catch (error) {
+      this.logger.error(error)
+      throw new InternalServerErrorException('Error al generar el token')
+    }
   }
 }
