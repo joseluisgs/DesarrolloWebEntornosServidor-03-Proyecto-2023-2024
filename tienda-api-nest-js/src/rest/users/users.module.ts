18498c8544b52d15e07a341184e93a9d1ffed648
José Luis González Sánchez <joseluisgonsan@gmail.com>
2023-11-17 10:04:48 +0100

    format prettier

--- tienda-api-nest-js/src/rest/users/users.module.ts
+++ tienda-api-nest-js/src/rest/users/users.module.ts
@@ -1,24 +1,23 @@
-import {Module} from '@nestjs/common'
-import {UsersService} from './users.service'
-import {UsersController} from './users.controller'
-import {TypeOrmModule} from '@nestjs/typeorm'
-import {Usuario} from './entities/user.entity'
-import {UserRole} from './entities/user-role.entity'
-import {UsuariosMapper} from './mappers/usuarios.mapper'
-import {CacheModule} from '@nestjs/cache-manager'
-import {BcryptService} from './bcrypt.service'
-import {PedidosModule} from "../pedidos/pedidos.module";
+import { Module } from '@nestjs/common'
+import { UsersService } from './users.service'
+import { UsersController } from './users.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Usuario } from './entities/user.entity'
+import { UserRole } from './entities/user-role.entity'
+import { UsuariosMapper } from './mappers/usuarios.mapper'
+import { CacheModule } from '@nestjs/cache-manager'
+import { BcryptService } from './bcrypt.service'
+import { PedidosModule } from '../pedidos/pedidos.module'
 
 @Module({
-    imports: [
-        TypeOrmModule.forFeature([Usuario]), // Importamos el repositorio de usuarios
-        TypeOrmModule.forFeature([UserRole]), // Importamos el repositorio de roles
-        CacheModule.register(), // Importamos el módulo de cache
-        PedidosModule,
-    ],
-    controllers: [UsersController],
-    providers: [UsersService, UsuariosMapper, BcryptService],
-    exports: [UsersService],
+  imports: [
+    TypeOrmModule.forFeature([Usuario]), // Importamos el repositorio de usuarios
+    TypeOrmModule.forFeature([UserRole]), // Importamos el repositorio de roles
+    CacheModule.register(), // Importamos el módulo de cache
+    PedidosModule,
+  ],
+  controllers: [UsersController],
+  providers: [UsersService, UsuariosMapper, BcryptService],
+  exports: [UsersService],
 })
-export class UsersModule {
-}
+export class UsersModule {}
