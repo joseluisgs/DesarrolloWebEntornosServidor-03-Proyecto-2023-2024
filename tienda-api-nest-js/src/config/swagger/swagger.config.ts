47ba2de0388806d51189e20262cb84f91cbec3a8
José Luis González Sánchez <joseluisgonsan@gmail.com>
2023-11-19 10:17:53 +0100

    refactor swagger y ssl

---
+++ tienda-api-nest-js/src/config/swagger/swagger.config.ts
@@ -0,0 +1,26 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { INestApplication } from '@nestjs/common'
+
+export function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('API REST Tienda Nestjs DAW 2023/2024')
+    .setDescription(
+      'API de ejemplo del curso Desarrollo de un API REST con Nestjs para 2º DAW. 2023/2024',
+    )
+    .setContact(
+      'José Luis González Sánchez',
+      'https://joseluisgs.dev',
+      'joseluis.gonzalez@iesluisvives.org',
+    )
+    .setExternalDoc(
+      'Documentación de la API',
+      'https://github.com/joseluisgs/DesarrolloWebEntornosServidor-03-2023-2024',
+    )
+    .setLicense('CC BY-NC-SA 4.0', 'https://joseluisgs.dev/docs/license/')
+    .setVersion('1.0.0')
+    .addTag('Productos', 'Operaciones con productos')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+}
--- tienda-api-nest-js/src/main.ts
+++ tienda-api-nest-js/src/main.ts
@@ -2,46 +2,17 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import * as process from 'process'
 import { ValidationPipe } from '@nestjs/common'
-import { readFileSync } from 'fs'
-import * as path from 'path'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { setupSwagger } from './config/swagger/swagger.config'
+import { getSSLOptions } from './config/ssl/ssl.config'
 
 async function bootstrap() {
-  // Leemos la configuración de los certificados SSL
-  const httpsOptions = {
-    key: readFileSync(path.resolve(process.env.SSL_KEY)),
-    cert: readFileSync(path.resolve(process.env.SSL_CERT)),
-  }
+  // Obtener las opciones de SSL
+  const httpsOptions = getSSLOptions()
   const app = await NestFactory.create(AppModule, { httpsOptions })
-  // Swagger
-  // Metadatos de la API
+  // Configuración de Swagger
   if (process.env.NODE_ENV === 'dev') {
-    const config = new DocumentBuilder()
-      .setTitle('API REST Tienda Nestjs DAW 2023/2024')
-      .setDescription(
-        'API de ejemplo del curso Desarrollo de un API REST con Nestjs para 2º DAW. 2023/2024',
-      )
-      .setContact(
-        'José Luis González Sánchez',
-        'https://joseluisgs.dev',
-        'joseluis.gonzalez@iesluisvives.org',
-      )
-      .setExternalDoc(
-        'Documentación de la API',
-        'https://github.com/joseluisgs/DesarrolloWebEntornosServidor-03-2023-2024',
-      )
-      /*.setExternalDoc(
-        'GitHub del proyecto',
-        'https://github.com/joseluisgs/DesarrolloWebEntornosServidor-03-Proyecto-2023-2024',
-      )*/
-      .setLicense('CC BY-NC-SA 4.0', 'https://joseluisgs.dev/docs/license/')
-      .setVersion('1.0.0')
-      .addTag('Productos', 'Operaciones con productos')
-      .build()
-    const document = SwaggerModule.createDocument(app, config)
-    SwaggerModule.setup('api', app, document)
+    setupSwagger(app)
   }
-  // Lanzamos el servidor
   // Configuración de la versión de la API
   app.setGlobalPrefix(process.env.API_VERSION || 'v1')
   // Activamos las validaciones body y dtos
